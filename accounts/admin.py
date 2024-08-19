54867d16e13410c328a417b6b2678b1273465212
Hugo Belém <belem.hugo@hotmail.com>
2024-08-19 16:33:27 -0300

    disabled username change for non superusers

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -10,5 +11,14 @@ class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
     list_display = ['first_name', 'username', 'email', 'phone']
     readonly_fields = ['date_joined', 'last_login']
+
+    def get_form(self, request, obj, **kwargs) -> Any:
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['username'].disabled = True
+
+        return form
 
 admin.site.register(CustomUser, CustomUserAdmin)
